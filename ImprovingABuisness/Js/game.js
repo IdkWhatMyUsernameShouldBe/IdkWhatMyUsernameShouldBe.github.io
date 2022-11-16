fa8b06d2b6ba62fbf5eb3d8015fb073d1fc10874
IdkWhatMyUsernameShouldBe <dpartovi@eastsideprep.org>
2022-11-15 23:21:23 -0800

    worked on contact page

--- ImprovingABuisness/Js/game.js
+++ ImprovingABuisness/Js/game.js
@@ -1,3 +1,12 @@
+
+
+
+/*This Javascript creates the page numbers for the slideshows. It is all my own code 
+and it took me a very long time to figure out how to add a delay to the functions using 
+the seTimeout function. I added a delay so that you can't change the page numbers faster 
+than the Bootstrap carousel could change slides. Yes, I am aware that I could've manually 
+typed the page numbers, but I chose to callenge myself instead :). */
+
 let slideNumber = 1;
 
 let maxSlides = 10;
@@ -37,3 +46,50 @@ function minusSlideNum() {
 }
 
 
+/*Code for surveys*/
+
+let fname = "";
+let lname = "";
+
+let surveysFinished = 0;
+
+let practiceScore = 0;
+
+let enjoymentScore = 0;
+
+function saveNames() {
+    fname = document.getElementById("fname").value;
+    lname = document.getElementById("lname").value;
+    surveysFinished += 1;
+}
+
+function practiceScore() {
+    let dailyPractice = document.getElementById("daily").checked;
+    let weeklyPractice = document.getElementById("weekly").checked;
+    let rarePractice = document.getElementById("rarely").checked;
+
+    if (dailyPractice) {
+        practiceScore = 3;
+    } else if (weeklyPractice) {
+        practiceScore = 2;
+    } else if (rarePractice) {
+        practiceScore = 1;
+    }
+    surveysFinished += 1;
+}
+
+
+function playingScore() {
+    let rating = prompt("On a scale from 1 to 10, how much do you enjoy playing piano?")
+    enjoymentScore = parseInt(rating);
+    surveysFinished += 1;
+}
+
+function finalScore() {
+    let responseText = document.getElementById('response');
+    if (surveysFinished == 3) {
+        responseText.innerHTML = "Please finish all surveys before checking your results.";
+    } else {
+        responseText.innerHTML = "Please finish all surveys before checking your results.";
+    }
+}
